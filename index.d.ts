85ee0e5ef3c37807cbbcbb97705c0a0a66d8c4ca
blankcry <gajejames@gmail.com>
2022-11-28 13:17:00 +0100

    chore: types for package

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+declare module 'micro-node-service-registry-lib' {
+  export default function (config: object): ServiceRegistry;
+
+  export interface ServiceRegistry {
+    register(object: object, callback: (error: Error, param: ServiceAttributes) => void): void;
+    unregister(id: string, name: string, callback: (error: Error, param: ServiceAttributes) => void): void;
+    find(name: string, endpoint: string): Promise<ServiceAttributes>;
+  }
+
+  interface ServiceAttributes {
+    'serviceId': string;
+    'serviceHost': string;
+    'servicePort': number;
+    'serviceProtocol': HTTPProtocol;
+    'endpointId': string;
+    'endpointPath': string;
+    'endpointUrl': string;
+    'domain': string;
+    'ts':Date;
+  }
+
+  interface ServiceConfig {
+    serviceId: string;
+    serviceHost: string;
+    servicePort: number;
+    endpointId: string,
+    endpointPath: string,
+    serviceProtocol: HTTPProtocol
+  }
+
+  type HTTPProtocol = 'http' | 'https';
+}
